7869af12aabb07c10c1e5ec4c7ffc72dee0e49e1
patricio1984 <61564880+patricio1984@users.noreply.github.com>
2020-05-14 02:43:03 -0300

    Adding the js scripts
    Progress bar animated and the number animation

--- js/progress-bar.js
+++ js/progress-bar.js
@@ -1,1 +1,7 @@
-
+$('.barra-nivel').each(function() {
+    var valorLargura = $(this).data('nivel');
+    var valorNivel = $(this).html("<span class='valor-nivel'>"+valorLargura+"</span>");
+      $(this).animate({
+          width: valorLargura
+      });
+  });
