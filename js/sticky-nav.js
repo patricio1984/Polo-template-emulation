576a8d0a30e499ec9f58b4fa72ee93800bbe2fb8
patricio1984 <61564880+patricio1984@users.noreply.github.com>
2020-05-23 16:52:00 -0300

    Correcting sticky nav and adding back to top js

--- js/sticky-nav.js
+++ js/sticky-nav.js
@@ -1,11 +1,11 @@
-// When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
-
 // Get the header
 var header = document.getElementById('myHeader');
 
 // Get the offset position of the navbar
 var sticky = header.offsetTop;
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
 
 // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
