33f6af6e22ace5bc162778c5a16abcc2805d751d
patricio1984 <61564880+patricio1984@users.noreply.github.com>
2020-07-30 03:30:58 -0300

    Fixing the back to top function

--- js/back-to-top-button.js
+++ js/back-to-top-button.js
@@ -14,6 +14,8 @@ function scrollFunction() {
 }
 
 // When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", topFunction);
+
 function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
